0e9a0dd70b28ad1ed3619956dafcbd53e67b7a36
unknown <tyu.sam@gmail.com>
2014-07-14 12:20:57 -0400

    Initial Commit

---
+++ install.bat
@@ -0,0 +1,2 @@
+move /Y snap.cmd C:\ 
+move /Y SNAPEX~1.lnk %userprofile%\Desktop\Snap.exe.lnk
---
+++ snap.cmd
@@ -0,0 +1,29 @@
+@echo off
+echo Successfully started SnapCMD 2014, use snaphelp for a commands list and usage!
+echo To extend Snap, create a .cmd file in C:\snap-plugins and hack away!
+cd C:\
+DOSKEY ls=dir
+DOSKEY clear=cls
+DOSKEY subl="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
+DOSKEY ~=cd C:\
+DOSKEY ~= cd C:\$Tcd $1
+DOSKEY ~u=cd %userprofile%$Tcd $1
+DOSKEY ..=@echo off$Tcd ..$Tfor /l %%x in (2, 1, $1) do cd ..$T@echo on
+
+DOSKEY snaphelp=@echo off$T^
+echo.$T^
+echo   HELP:$T^
+echo.$T^
+echo       ls            - List contents of the current directory info$T^
+echo       clear         - Clears the console$T^
+echo       subl          - Opens the file in sublime text command line editor$T^
+echo       ~ [path]      - cd to C:\[path]$T^
+echo       ~u [path]     - cd to C:\Users\USER\[path]$T^
+echo       .. [n]        - cd to [n]th super directory shortcut$T^
+echo       snaphelp      - Displays this help dialog$T^
+@echo on
+
+if not exist C:\snap-plugins mkdir C:\snap-plugins
+
+for %%v in ("C:\snap-plugins\*") do call "%%~v"
+
